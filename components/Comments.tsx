8beca26d2d9abfc4e0d1142de8d8499e22a8fbfe
Pedro Bernardi <pedrobealves@gmail.com>
2023-08-10 22:31:43 -0300

    Update share url

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -7,6 +7,8 @@ interface CommentsProps {
 }
 
 export default function Comments({ slug, identifier, title }: CommentsProps) {
+  console.log(`${process.env.NEXT_PUBLIC_NEXTJS_SITE_URL}/post/${slug}`)
+
   return (
     <DiscussionEmbed
       shortname="miltensei"
