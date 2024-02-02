5d644d316e003b91bccd1989a2518b5cf5aa0c9a
pedrobealves <pedrobealves@gmail.com>
2024-02-02 20:36:34 -0300

    Update styles for portableText elements

--- features/post/components/page/PostTitle.tsx
+++ features/post/components/page/PostTitle.tsx
@@ -4,7 +4,7 @@ interface PostTitleProps {
 
 export default function PostTitle({ children }: PostTitleProps) {
   return (
-    <h1 className="text-4xl md:text-6xl font-bold text-primary-8 mb-6 break-words">
+    <h1 className="text-4xl md:text-6xl font-bold text-primary-8 mb-6 sm:break-normal break-words break-all">
       {children}
     </h1>
   )
