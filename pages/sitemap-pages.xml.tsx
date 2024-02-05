bf15f0e0aae34ae49e3b299c3f7500f05f2558a3
pedrobealves <pedrobealves@gmail.com>
2024-02-04 22:51:59 -0300

    Refactor sitemap generation code

---
+++ pages/sitemap-pages.xml.tsx
@@ -0,0 +1,104 @@
+import { getAll, getClient } from 'lib/sanity.client'
+import { Category, Tag } from 'lib/sanity.queries'
+
+const contentTypes = [
+  { type: 'tag', path: '/tag/', priority: 0.7, changefreq: 'hourly' },
+  { type: 'category', path: '/', priority: 0.7, changefreq: 'daily' },
+]
+
+type SitemapLocation = {
+  url: string
+  changefreq?:
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never'
+  priority: number
+  lastmod?: Date
+}
+
+// Use this to manually add routes to the sitemap
+const defaultUrls: SitemapLocation[] = [
+  {
+    url: '/',
+    changefreq: 'hourly',
+    priority: 1,
+    lastmod: new Date(), // or custom date: '2023-06-12T00:00:00.000Z',
+  },
+  //   { url: '/about', priority: 0.5 },
+  //   { url: '/blog', changefreq: 'weekly', priority: 0.7 },
+]
+
+const createSitemap = (locations: SitemapLocation[]) => {
+  const baseUrl = process.env.NEXT_PUBLIC_NEXTJS_SITE_URL // Make sure to configure this
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${locations
+        .map((location) => {
+          return `<url>
+                    <loc>${baseUrl}${location.url}</loc>
+                    <changefreq>${location.changefreq || 'weekly'}</changefreq>
+                    <priority>${location.priority}</priority>
+                    ${
+                      location.lastmod
+                        ? `<lastmod>${location.lastmod.toISOString()}</lastmod>`
+                        : ''
+                    }
+                  </url>`
+        })
+        .join('')}
+  </urlset>
+  `
+}
+
+export default function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+// Função para obter URLs
+async function getUrls<T extends { slug?: string; _updatedAt?: string }>(
+  client: any,
+  contentType: {
+    type: string
+    path: string
+    priority: number
+    changefreq: string
+  },
+) {
+  const [items = []] = await Promise.all([getAll<T>(client, contentType.type)])
+  return items
+    .filter(({ slug = '' }) => slug)
+    .map((item) => {
+      return {
+        url: `${contentType.path}${item.slug}`,
+        priority: contentType.priority,
+        lastmod: new Date(item._updatedAt),
+        changefreq: contentType.changefreq,
+      }
+    })
+}
+
+export async function getServerSideProps({ res }) {
+  const client = getClient()
+
+  // Return the default urls, combined with dynamic urls above
+  const postUrls = []
+
+  for (const contentType of contentTypes) {
+    const tagsURL = await getUrls<Tag | Category>(client, contentType)
+    postUrls.push(...tagsURL)
+  }
+
+  const locations = [...defaultUrls, ...postUrls]
+
+  // Set response to XML
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(createSitemap(locations))
+  res.end()
+
+  return {
+    props: {},
+  }
+}
--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,109 +1,46 @@
 import { getAll, getClient } from 'lib/sanity.client'
 import { Category, Tag } from 'lib/sanity.queries'
 
-const contentTypes = [
-  { type: 'tag', path: '/tag/', priority: 0.7, changefreq: 'hourly' },
-  { type: 'category', path: '/', priority: 0.7, changefreq: 'daily' },
-]
-
-type SitemapLocation = {
+type Sitemap = {
   url: string
-  changefreq?:
-    | 'always'
-    | 'hourly'
-    | 'daily'
-    | 'weekly'
-    | 'monthly'
-    | 'yearly'
-    | 'never'
-  priority: number
-  lastmod?: Date
 }
 
 // Use this to manually add routes to the sitemap
-const defaultUrls: SitemapLocation[] = [
-  {
-    url: '/',
-    changefreq: 'hourly',
-    priority: 1,
-    lastmod: new Date(), // or custom date: '2023-06-12T00:00:00.000Z',
-  },
+const defaultUrls: Sitemap[] = [
   {
     url: '/sitemap-news.xml',
-    changefreq: 'hourly',
-    priority: 1,
-    lastmod: new Date(), // or custom date: '2023-06-12T00:00:00.000Z',
   },
   {
     url: '/sitemap-posts.xml',
-    changefreq: 'daily',
-    priority: 1,
-    lastmod: new Date(), // or custom date: '2023-06-12T00:00:00.000Z',
   },
-  //   { url: '/about', priority: 0.5 },
-  //   { url: '/blog', changefreq: 'weekly', priority: 0.7 },
+  {
+    url: '/sitemap-pages.xml',
+  },
 ]
 
-const createSitemap = (locations: SitemapLocation[]) => {
+const createSitemap = (locations: Sitemap[]) => {
   const baseUrl = process.env.NEXT_PUBLIC_NEXTJS_SITE_URL // Make sure to configure this
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${locations
-        .map((location) => {
-          return `<url>
-                    <loc>${baseUrl}${location.url}</loc>
-                    <changefreq>${location.changefreq || 'weekly'}</changefreq>
-                    <priority>${location.priority}</priority>
-                    ${
-                      location.lastmod
-                        ? `<lastmod>${location.lastmod.toISOString()}</lastmod>`
-                        : ''
-                    }
-                  </url>`
-        })
-        .join('')}
-  </urlset>
-  `
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${locations
+    .map((location) => {
+      return `
+        <sitemap>
+          <loc>${location.url}</loc>
+        </sitemap>
+        `
+    })
+    .join('')}
+        </sitemapindex>
+        `
 }
 
 export default function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
-// Função para obter URLs
-async function getUrls<T extends { slug?: string; _updatedAt?: string }>(
-  client: any,
-  contentType: {
-    type: string
-    path: string
-    priority: number
-    changefreq: string
-  },
-) {
-  const [items = []] = await Promise.all([getAll<T>(client, contentType.type)])
-  return items
-    .filter(({ slug = '' }) => slug)
-    .map((item) => {
-      return {
-        url: `${contentType.path}${item.slug}`,
-        priority: contentType.priority,
-        lastmod: new Date(item._updatedAt),
-        changefreq: contentType.changefreq,
-      }
-    })
-}
 
 export async function getServerSideProps({ res }) {
-  const client = getClient()
-
-  // Return the default urls, combined with dynamic urls above
-  const postUrls = []
-
-  for (const contentType of contentTypes) {
-    const tagsURL = await getUrls<Tag | Category>(client, contentType)
-    postUrls.push(...tagsURL)
-  }
-
-  const locations = [...defaultUrls, ...postUrls]
+  const locations = [...defaultUrls]
 
   // Set response to XML
   res.setHeader('Content-Type', 'text/xml')
