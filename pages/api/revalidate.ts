a22d1b922d0cfaa444a5212b2e2aa7e14f510a58
pedrobealves <pedrobealves@gmail.com>
2024-02-06 23:37:28 -0300

    Fix queryStaleRoutes function to handle 'tag' type

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -103,6 +103,8 @@ async function queryStaleRoutes(
       return await queryAllRoutes(client, TYPES)
     case 'category':
       return await queryStaleCategoryRoutes(client, body._id, 'category')
+    case 'category':
+      return await queryStaleCategoryRoutes(client, body._id, 'tag')
     default:
       throw new TypeError(`Unknown type: ${body._type}`)
   }
